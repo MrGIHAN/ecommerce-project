248d0bf4936cc9ed422f4f5e9b983accfad243c9
Chamod Gihan Pathirana <gihanpathirana84@gmail.com>
2024-08-13 22:17:25 +0530

    Shop category final

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import './CSS/ShopCategory.css';
+import { ShopContext } from '../Context/ShopContext';  
+import droupdown_icon from '../Components/Assets/dropdown_icon.png';
+import Item from '../Components/Item/Item'; // Capitalized "Item"
 
-export default function ShopCategory() {
+export default function ShopCategory(props) {
+  const { all_products } = useContext(ShopContext);
+
   return (
-    <div>
-      
+    <div className='shop-category'>
+      <img className='shopcategory-banner' src={props.banner} alt="banner" /> {/* Updated className */}
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={droupdown_icon} alt="dropdown_icon" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_products.map((item, i) => {
+          if (props.category === item.category) {
+            return (
+              <Item 
+                key={i} 
+                id={item.id} 
+                name={item.name} 
+                image={item.image}
+                new_price={item.new_price}
+                old_price={item.old_price}
+              />
+            );
+          } else {
+            return null;
+          }
+        })}
+      </div>
     </div>
-  )
+  );
 }
